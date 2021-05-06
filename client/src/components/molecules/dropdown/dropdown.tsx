9e9bd816542019cdf869fa4912b03ca7f822c379
Krystian <40540225+KrysOlechowski@users.noreply.github.com>
2021-05-06 08:46:36 +0200

    change dropdown text color

--- client/src/components/molecules/dropdown/dropdown.tsx
+++ client/src/components/molecules/dropdown/dropdown.tsx
@@ -1,94 +1,100 @@
-import clsx from 'clsx'
-import { FC, useCallback, useState } from 'react';
-import { Button, Wrapper as DropdownWrapper, Menu, MenuItem } from 'react-aria-menubutton';
+import clsx from "clsx";
+import { FC, useCallback, useState } from "react";
+import {
+  Button,
+  Wrapper as DropdownWrapper,
+  Menu,
+  MenuItem,
+} from "react-aria-menubutton";
 
 import styled from "styled-components/macro";
 
-import '../../../theme/variables.scss'
-import { ArrowDownIcon } from '../../../assets/icons'
-
+import { ArrowDownIcon } from "../../../assets/icons";
+import "../../../theme/variables.scss";
 interface Props {
-   options: { label: string }[]
-   onSelection: (value: string) => void;
-   className?: string;
+  options: { label: string }[];
+  onSelection: (value: string) => void;
+  className?: string;
 }
 
-
-
 export const Dropdown: FC<Props> = ({ options, onSelection, className }) => {
-   const [value, setValue] = useState(options[0].label)
-   const [isOpen, setIsOpen] = useState(false);
+  const [value, setValue] = useState(options[0].label);
+  const [isOpen, setIsOpen] = useState(false);
 
-   const onMenuToggle = (e: { isOpen: boolean | ((prevState: boolean) => boolean); }) => {
-      setIsOpen(e.isOpen);
-   };
+  const onMenuToggle = (e: {
+    isOpen: boolean | ((prevState: boolean) => boolean);
+  }) => {
+    setIsOpen(e.isOpen);
+  };
 
-   const handleSelection = useCallback((e) => {
-      setValue(e)
-      onSelection(e)
-   }, [onSelection])
+  const handleSelection = useCallback(
+    (e) => {
+      setValue(e);
+      onSelection(e);
+    },
+    [onSelection]
+  );
 
-   return (
-      <Container className={clsx("wrapper", className)}>
-         <DropdownWrapper
-            className="dropdown"
-            onSelection={handleSelection}
-            onMenuToggle={onMenuToggle}
-         >
-            <Button className="dropdown-title">
-               <div>{value}</div>
-               <div>
-                  <ArrowDownIcon className={clsx('dropdown-arrow', { 'rotate': isOpen })} />
-               </div>
-            </Button>
-            <Menu className="dropdown-menu-wrapper">
-               <ul className="dropdown-menu">
-                  {options.map((opt, i) => {
-                     return (
-                        <MenuItem
-                           value={opt.label}
-                           className='dropdown-menu-item'
-                           key={i}
-                        >
-                           <li className="menu-item-wrapper">
-                              <div className="menu-item-inner">
-                                 <div> {opt.label} </div>
-
-                              </div>
-                           </li>
-                        </MenuItem>
-                     );
-                  })}
-               </ul>
-            </Menu>
-         </DropdownWrapper>
-      </Container>
-   )
+  return (
+    <Container className={clsx("wrapper", className)}>
+      <DropdownWrapper
+        className="dropdown"
+        onSelection={handleSelection}
+        onMenuToggle={onMenuToggle}
+      >
+        <Button className="dropdown-title">
+          <div>{value}</div>
+          <div>
+            <ArrowDownIcon
+              className={clsx("dropdown-arrow", { rotate: isOpen })}
+            />
+          </div>
+        </Button>
+        <Menu className="dropdown-menu-wrapper">
+          <ul className="dropdown-menu">
+            {options.map((opt, i) => {
+              return (
+                <MenuItem
+                  value={opt.label}
+                  className="dropdown-menu-item"
+                  key={i}
+                >
+                  <li className="menu-item-wrapper">
+                    <div className="menu-item-inner">
+                      <div> {opt.label} </div>
+                    </div>
+                  </li>
+                </MenuItem>
+              );
+            })}
+          </ul>
+        </Menu>
+      </DropdownWrapper>
+    </Container>
+  );
 };
 
 const Container = styled.div`
-
-.dropdown{
-   border: 1px solid #3F3F3F;
+  .dropdown {
+    border: 1px solid #3f3f3f;
     text-align-last: left;
     background-color: white;
     border-radius: 10px;
-    border-width:2px;
+    border-width: 2px;
     position: relative;
+  }
 
-}
-
-.dropdown-title{
-   outline:none;
-   position: relative;
+  .dropdown-title {
+    outline: none;
+    position: relative;
     display: flex;
     padding: 5px;
     color: var(--abbey);
     max-width: 260px;
-}
+  }
 
-.dropdown-arrow{
-   position: absolute;
+  .dropdown-arrow {
+    position: absolute;
     width: 1.8em;
     pointer-events: none;
     right: 5px;
@@ -96,41 +102,42 @@ const Container = styled.div`
     transform: translateY(-50%) translateX(-50%) rotate(-90deg);
     transition: all 0.3s ease;
 
-    &.rotate{
+    &.rotate {
       transform: translateY(-50%) translateX(-50%) rotate(0);
     }
-}
+  }
 
-.dropdown-menu-wrapper{
-   outline: none;
+  .dropdown-menu-wrapper {
+    outline: none;
     position: absolute;
     z-index: 999999;
     width: 100%;
     max-width: 260px;
     box-sizing: border-box;
 
-    .dropdown-menu{
+    .dropdown-menu {
       list-style-type: none;
       margin: 0;
       padding: 0;
 
-    .dropdown-menu-item{
-      outline: none;
-      border: 1px solid #3f3f3f;
-    border-width: 2px;
-    border-radius: 10px;
-    background: white;
+      .dropdown-menu-item {
+        color: var(--abbey);
+        outline: none;
+        border: 1px solid #3f3f3f;
+        border-width: 2px;
+        border-radius: 10px;
+        background: white;
 
-      &:hover{
-         background-color: var(--gallery);
-         cursor: pointer;
-      }
+        &:hover {
+          background-color: var(--gallery);
+          cursor: pointer;
+        }
 
-      .menu-item-wrapper{
-         padding: 10px 15px;
-         color: inherit;
+        .menu-item-wrapper {
+          padding: 10px 15px;
+          color: inherit;
+        }
       }
     }
-    }
-}
-`
+  }
+`;
